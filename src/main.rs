7f2b0028f3bd3410a813822ba1c2ce51d0ffa1bc
Joseph Levine <joseph.levine@senate.mn>
2019-09-13 12:03:42 -0500

    added help message

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+use std::env;
+
+fn help() {
+    println!("usage: up-all [options]
+
+Options:
+
+  --cache        Force cache file use
+  --no-cache     Disable cache file use
+  --config=FILE  If specified, use the given file as the up-all configuration
+
+Fly, you fools!
+")
+}
+
 fn main() {
-    println!("Hello, world!");
+    // Take a look at this: https://github.com/BurntSushi/xsv/blob/master/src/main.rs
+    // parse args / flags
+
+    // open cache & config
+
+    // run package managers
+
+    // store results to cache
 }
